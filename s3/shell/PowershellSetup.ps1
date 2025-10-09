5a1ec08d063906ea3dbf9c99c3911277a51502b2
jmenezesdev-git <55636286+jmenezesdev-git@users.noreply.github.com>
2025-10-09 17:53:00 -0400

    Writing some basic scripts for AWS s3
    mostly bash/linux commands. did a bit of powershell

---
+++ s3/shell/PowershellSetup.ps1
@@ -0,0 +1,9 @@
+﻿##Needed to setup AWS in powershell
+Install-Module -Name AWS.Tools.Installer
+Install-AWSToolsModule AWS.Tools.S3 -CleanUp
+Set-ExecutionPolicy RemoteSigned 
+## update command, run periodically
+#Update-AWSToolsModule -CleanUp
+
+#Specify user
+Set-AWSCredential -ProfileName #MYUSERNAMEHERE##
